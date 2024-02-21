58a33ff3c6709d18894674352d67a0f1f9063fa2
Celestine Kariuki <celestino@safariblocks.com>
2024-02-21 09:41:01 +0000

    controller

---
+++ controllers.go/patient.controller.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"fmt"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+)
+
+type PatientController struct{
+	databaseSession *mgo.Session
+}
+
+func NewPatientController(databaseSession *mgo.Session) *PatientController{
+
+	return &PatientController{databaseSession}
+}
+
+GetPatient
+
+CreatePatient
+
+DeletePatient
